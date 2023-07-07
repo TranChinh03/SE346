14d28227dbe2832a13bd99a5dc257c06b68cf4a2
21522611@gm.uit.edu.vn <21522611@gm.uit.edu.vn>
2023-07-07 14:01:14 +0700

    dev: delete Material in add lesson

--- CHTapp/App.js
+++ CHTapp/App.js
@@ -82,6 +82,5 @@ export default function App() {
     //<RatingScreen />
     // <EditChapterScreen />
     // <EditLessonScreen />
-
   );
 }
--- CHTapp/src/components/ItemPdf.js
+++ CHTapp/src/components/ItemPdf.js
@@ -1,8 +1,9 @@
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
 import React, {Component} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import scale from '../constants/responsive';
 import CUSTOM_COLORS from '../constants/colors';
+import BtnDelete from './BtnDelete';
 
 export default class ItemPdf extends Component {
   constructor(props) {
@@ -17,7 +18,14 @@ export default class ItemPdf extends Component {
       <SafeAreaView>
         <View style={styles.conPdf}>
           <View style={styles.conDecor}></View>
-          <Text style={styles.txtPdf}>{this.props.title}</Text>
+          <Text style={styles.txtPdf} numberOfLines={1}>
+            {this.props.title}
+          </Text>
+          <TouchableOpacity
+            style={styles.btnBorder}
+            onPress={this.props.onPress}>
+            <Text style={styles.txtDelete}>-</Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     );
@@ -28,7 +36,7 @@ const styles = StyleSheet.create({
   conPdf: {
     marginHorizontal: scale(15, 'w'),
     height: scale(45, 'h'),
-    width: scale(150, 'h'),
+    width: scale(160, 'h'),
     borderRadius: scale(5, 'h'),
     borderWidth: scale(1, 'h'),
     borderColor: CUSTOM_COLORS.usBlue,
@@ -43,5 +51,22 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginLeft: scale(10, 'w'),
     textDecorationLine: 'underline',
+    width: scale(100, 'w'),
+  },
+  btnBorder: {
+    height: scale(30, 'h'),
+    width: scale(30, 'h'),
+    borderRadius: scale(30 / 2, 'h'),
+    borderWidth: scale(1, 'w'),
+    borderColor: CUSTOM_COLORS.sunsetOrange,
+    backgroundColor: CUSTOM_COLORS.sunsetOrange,
+    //padding: 0,
+    justifyContent: 'center',
+  },
+  txtDelete: {
+    fontSize: scale(20, 'w'),
+    //fontWeight: 100,
+    color: CUSTOM_COLORS.white,
+    alignSelf: 'center',
   },
 });
